97812a0edf8f605d80c798003093be1878192415
bcdxn <bcdixon@outlook.com>
2015-06-08 17:44:06 -0400

    gulpfile.js boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp       = require('gulp'),
+    uglify     = require('uglify'),
+    source     = require('vinyl-source-stream'),
+    browserify = require('browserify'),
+    streamify  = require('gulp-streamify'),
+    concat     = require('gulp-concat'),
+    cleanCss   = require('less-plugin-clean-css'),
+    autoprefix = require('less-plugin-autoprefix'),
+    rename     = require('gulp-rename'),
+    watch      = require('gulp-watch'),
+    path       = {};
+
+path.LESS        = './src/less/**/*.less';
+path.JS          = './src/js/**/*.js';
+path.JS_ENTRY    = './src/js/index.js';
+path.DIST        = './dist';
+path.OUT_CSS     = 'bundle.css';
+path.OUT_JS      = 'bundle.js';
+path.OUT_CSS_MIN = 'bundle.min.css';
+path.OUT_JS_MIN  = 'bundle.min.js';
+
+// CSS
+gulp.task('css', function () {
+  
+});
+
+// JS
+gulp.task('js', function () {
+  
+});
+
+// Watch CSS
+gulp.task('watchCss', function () {
+  gulp.watch(path.CSS, ['css']);
+});
+
+// Watch JS
+gulp.task('watchJs', function () {
+  gulp.watch(path.CSS, ['js']);
+});
+
+gulp.task('default', ['css', 'js', 'watchCss', 'watchJs']);
